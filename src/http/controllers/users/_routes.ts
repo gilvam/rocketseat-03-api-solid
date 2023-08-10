02616ebe0fde6e554ef12b10fac078785b8c4340
Gilvam Mourao <gilvamm@gmail.com>
2023-08-10 15:56:46 +1000

    feat: refresh token

--- src/http/controllers/users/_routes.ts
+++ src/http/controllers/users/_routes.ts
@@ -4,10 +4,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { registerController } from './register.controller'
 import { authenticateController } from './authenticate.controller'
 import { profileController } from './profile.controller'
+import { refreshController } from '@/http/controllers/users/refresh.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
 	app.post('/users', registerController)
 	app.post('/sessions', authenticateController)
+
+	app.patch('/token/refresh', refreshController)
 
 	/** Authenticated */
 	app.get('/me', { onRequest: [verifyJWT] }, profileController)
